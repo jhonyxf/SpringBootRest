677d0722284f5352d24dc9666493151cd32dc6af
jhonyxf1 <jhonyxf1@gmail.com>
2020-01-04 11:39:07 -0300

    Implementando uma soma simples

--- src/main/java/br/com/sbr/HelloController.java
+++ src/main/java/br/com/sbr/HelloController.java
@@ -3,7 +3,9 @@ package br.com.sbr;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,43 @@ public class HelloController {
 	public Hello ola(@RequestParam(value="nome", defaultValue="Jow") String nome) {
 		return new Hello(counter.incrementAndGet(), String.format(template, nome));
 	}
+	
+	@RequestMapping(value="/somar/{numeroUm}/{numeroDois}", method=RequestMethod.GET)
+	public Double somar(@PathVariable("numeroUm") String numeroUm, @PathVariable("numeroDois") String numeroDois) throws Exception {
+		if(!isNumero(numeroUm) || !isNumero(numeroDois)) {
+			throw new Exception();
+		}
+		
+		return convertToDouble(numeroUm) + convertToDouble(numeroDois);
+	}
+	
+	private Double convertToDouble(String strNumero) {
+		if(strNumero == null) return 0D;
+		String numero = strNumero.replaceAll(",", ".");
+		if(isNumero(numero)) return Double.parseDouble(numero);
+		return 0D;
+	}
+
+	private boolean isNumero(String strNumero) {
+		if(strNumero == null) return false;
+		String numero = strNumero.replaceAll(",", ".");
+		return numero.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
